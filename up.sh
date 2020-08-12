88c78758b503763c9f14e6153a0adf2393600c37
AmazingCat320 <francesco.strmih@gmail.com>
2020-08-12 17:50:04 +0300

    Automatic save commit initiated at Wed Aug 12 17:50:04 EEST 2020

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git add ./up.sh
+git add  --pathspec-from-file up.sh
 git commit -m "Automatic save commit initiated at $(date)"
 git push https://AmazingCat320:Falcon211006@github.com/AmazingCat320/hugosite.git master
